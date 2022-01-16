88d7cb070192367457e97c60b3e2fe3b0ae493e5
mAbduqayum <mabduqayum@gmail.com>
2022-01-16 15:34:04 +0500

    minor changes

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 // todo: use strict
 
 let mainNav = document.querySelectorAll('.main-nav-item > a')
-let activeNav = 0;
+let activeNavIndex = 0;
 
 for (let i = 0; i < mainNav.length; i++) {
     const mainNavElement = mainNav[i];
     mainNavElement.addEventListener('click', () => {
         if (!mainNavElement.hasAttribute('class=active')) {
-            mainNav[activeNav].classList.remove('active')
+            mainNav[activeNavIndex].classList.remove('active')
             mainNavElement.classList.add('active');
-            activeNav = i;
+            activeNavIndex = i;
         }
     })
 }
